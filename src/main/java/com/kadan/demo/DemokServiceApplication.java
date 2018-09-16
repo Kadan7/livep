83bed885489bf4f7de299471b7cd2a9fee581877
kadan.li <kadan.li@bamboonetworks.com>
2018-09-16 21:44:08 +0800

    circuit first commit

--- src/main/java/com/kadan/demo/DemokServiceApplication.java
+++ src/main/java/com/kadan/demo/DemokServiceApplication.java
@@ -1,13 +1,15 @@
 package com.kadan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class DemokServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kadan/demo/controller/ClientController.java
+++ src/main/java/com/kadan/demo/controller/ClientController.java
@@ -6,38 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedHashMap;
-
 
 @RestController
 @RequestMapping("/")
-public class DemoRestController {
+public class ClientController {
 
-    private final static Logger logger = LoggerFactory.getLogger(DemoRestController.class);
+    private final static Logger logger = LoggerFactory.getLogger(ClientController.class);
 
     @Autowired
     RestTemplate restTemplate;
 
-    @Value("${client}")
-    String client;
-
-    @RequestMapping(value = "/circuit1", method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod = "defaultReturn")
-    public String getBookmarks() {
-        logger.info("visting circuit1 ...");
-        return restTemplate.getForObject(client,String.class);
-        //return ResponseEntity.status(HttpStatus.OK).body("you are visiting circuit service 1 ...");
-
-    }
-
+    @RequestMapping(value = "/client", method = RequestMethod.GET)
     public String defaultReturn(){
-        return "somethings' wrong in the downstream host ...";
+        return " here comes the client response >>>> :::::: ";
     }
 
 
